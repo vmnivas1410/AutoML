90d993a0fb9b683660e6e002fdde34a94d8b8aad
vmnivas1410 <55321803+vmnivas1410@users.noreply.github.com>
2021-06-07 13:56:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,165 @@
+
+import streamlit as st
+import pandas as pd
+from lazypredict.Supervised import LazyClassifier
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import seaborn as sns
+import base64
+import io
+
+#---------------------------------#
+# Page layout
+## Page expands to full width
+st.set_page_config(page_title='AutoML for Classification',
+    layout='wide')
+#---------------------------------#
+# Model building
+def build_model(df):
+    df = df.loc[:] # FOR TESTING PURPOSE, COMMENT THIS OUT FOR PRODUCTION
+    X = df.iloc[:,:-1] # Using all column except for the last column as X
+    Y = df.iloc[:,-1] # Selecting the last column as Y
+
+    st.markdown('**1.2. Dataset dimension**')
+    st.write('X')
+    st.info(X.shape)
+    st.write('Y')
+    st.info(Y.shape)
+
+    st.markdown('**1.3. Variable details**:')
+    st.write('X variable (first 20 are shown)')
+    st.info(list(X.columns[:20]))
+    st.write('Y variable')
+    st.info(Y.name)
+
+    # Build lazy model
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y,test_size = split_size,random_state = seed_number)
+    reg = LazyClassifier(verbose=0,ignore_warnings=False, custom_metric=None)
+    models_train,predictions_train = reg.fit(X_train, X_train, Y_train, Y_train)
+    models_test,predictions_test = reg.fit(X_train, X_test, Y_train, Y_test)
+
+    st.subheader('2. Table of Model Performance')
+
+    st.write('Training set')
+    st.write(predictions_train)
+    st.markdown(filedownload(predictions_train,'training.csv'), unsafe_allow_html=True)
+
+    st.write('Test set')
+    st.write(predictions_test)
+    st.markdown(filedownload(predictions_test,'test.csv'), unsafe_allow_html=True)
+
+    st.subheader('3. Plot of Model Performance (Test set)')
+
+    with st.markdown('**Accuracy**'):
+        # Tall
+        predictions_test["Accuracy"] = [0 if i < 0 else i for i in predictions_test["Accuracy"] ]
+        plt.figure(figsize=(3, 9))
+        sns.set_theme(style="whitegrid")
+        ax1 = sns.barplot(y=predictions_test.index, x="Accuracy", data=predictions_test)
+        ax1.set(xlim=(0, 1))
+    st.markdown(imagedownload(plt,'plot-accuracy-tall.pdf'), unsafe_allow_html=True)
+        # Wide
+    plt.figure(figsize=(9, 3))
+    sns.set_theme(style="whitegrid")
+    ax1 = sns.barplot(x=predictions_test.index, y="Accuracy", data=predictions_test)
+    ax1.set(ylim=(0, 1))
+    plt.xticks(rotation=90)
+    st.pyplot(plt)
+    st.markdown(imagedownload(plt,'plot-accuracy-wide.pdf'), unsafe_allow_html=True)
+
+    with st.markdown('**F1 Score**'):
+        # Tall
+        predictions_test["F1 Score"] = [50 if i > 50 else i for i in predictions_test["F1 Score"] ]
+        plt.figure(figsize=(3, 9))
+        sns.set_theme(style="whitegrid")
+        ax2 = sns.barplot(y=predictions_test.index, x="F1 Score", data=predictions_test)
+    st.markdown(imagedownload(plt,'plot-F1 Score-tall.pdf'), unsafe_allow_html=True)
+        # Wide
+    plt.figure(figsize=(9, 3))
+    sns.set_theme(style="whitegrid")
+    ax2 = sns.barplot(x=predictions_test.index, y="F1 Score", data=predictions_test)
+    plt.xticks(rotation=90)
+    st.pyplot(plt)
+    st.markdown(imagedownload(plt,'plot-F1 Score-wide.pdf'), unsafe_allow_html=True)
+
+    with st.markdown('**Calculation time**'):
+        # Tall
+        predictions_test["Time Taken"] = [0 if i < 0 else i for i in predictions_test["Time Taken"] ]
+        plt.figure(figsize=(3, 9))
+        sns.set_theme(style="whitegrid")
+        ax3 = sns.barplot(y=predictions_test.index, x="Time Taken", data=predictions_test)
+    st.markdown(imagedownload(plt,'plot-calculation-time-tall.pdf'), unsafe_allow_html=True)
+        # Wide
+    plt.figure(figsize=(9, 3))
+    sns.set_theme(style="whitegrid")
+    ax3 = sns.barplot(x=predictions_test.index, y="Time Taken", data=predictions_test)
+    plt.xticks(rotation=90)
+    st.pyplot(plt)
+    st.markdown(imagedownload(plt,'plot-calculation-time-wide.pdf'), unsafe_allow_html=True)
+
+
+# Download CSV data
+# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
+def filedownload(df, filename):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download={filename}>Download {filename} File</a>'
+    return href
+
+def imagedownload(plt, filename):
+    s = io.BytesIO()
+    plt.savefig(s, format='pdf', bbox_inches='tight')
+    plt.close()
+    b64 = base64.b64encode(s.getvalue()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:image/png;base64,{b64}" download={filename}>Download {filename} File</a>'
+    return href
+
+#---------------------------------#
+st.write("""
+# AutoML for Classification
+
+[ Developed by: Dinesh Kumar S, Nivas V M & Dr. Kanish T C ]
+
+Vellore Institute of Technology, Vellore.
+
+""")
+
+#---------------------------------#
+# Sidebar - Collects user input features into dataframe
+with st.sidebar.header('1. Upload your CSV data'):
+    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+    st.sidebar.markdown("""
+[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
+""")
+
+# Sidebar - Specify parameter settings
+with st.sidebar.header('2. Set Parameters'):
+    split_size = st.sidebar.slider('Data split ratio (% for Training Set)', 10, 90, 80, 5)
+    seed_number = st.sidebar.slider('Set the random seed number', 1, 100, 42, 1)
+
+
+#---------------------------------#
+# Main panel
+
+# Displays the dataset
+st.subheader('1. Dataset')
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.markdown('**1.1. Glimpse of dataset**')
+    st.write(df)
+    build_model(df)
+else:
+    st.info('Awaiting for CSV file to be uploaded.')
+    if st.button('Press to use Example Dataset'):        
+        dataset = load_breast_cancer()        
+        X = pd.DataFrame(dataset.data, columns=dataset.feature_names).loc[:] # FOR TESTING PURPOSE, COMMENT THIS OUT FOR PRODUCTION
+        Y = pd.Series(dataset.target, name='response').loc[:] # FOR TESTING PURPOSE, COMMENT THIS OUT FOR PRODUCTION
+        df = pd.concat( [X,Y], axis=1 )
+
+        st.markdown('The Breast Cancer dataset is used as the example.')
+        st.write(df.head(600))
+
+        build_model(df)
+
